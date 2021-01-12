57a33a4da9bf084d99feed754b6e7de5c14e265e
Matt Turner <matturner@gmail.com>
2021-01-12 22:38:11 +0000

    No longer want gogo

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 rm -rf api-common-protos && git clone git@github.com:googleapis/api-common-protos.git
 
 # protoc?
-go get -u google.golang.org/protobuf/cmd/protoc-gen-go
-go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
-go get -u github.com/googleapis/gapic-generator-go/cmd/protoc-gen-go_gapic
-go get -u github.com/gogo/protobuf/protoc-gen-gogo
+go get -u -v google.golang.org/protobuf/cmd/protoc-gen-go
+go get -u -v google.golang.org/grpc/cmd/protoc-gen-go-grpc
+go get -u -v github.com/googleapis/gapic-generator-go/cmd/protoc-gen-go_gapic
