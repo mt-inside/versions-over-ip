d8fc20f48885e0f0e501349511ca20820416093e
Matt Turner <m.turner@mwam.com>
2021-06-05 14:03:51 +0100

    l2 job control

--- build/quick.sh
+++ build/quick.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 ( go run cmd/server/server.go ) & pid=$!
+
 go run cmd/client/client.go
 
-echo pid is $pid
-ps $pid
-echo other "server"s
-pgrep server
-echo "go run forks?"
-
 echo killing $pid
-kill $pid
+kill -- -$pid # `go run` forks, so this kills the whole process group (the PID of the leader is the PGID of the group)
